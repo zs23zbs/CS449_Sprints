d9f4a40166303b96d2967183d946ac13409ab0e8
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-09-04 16:50:09 -0500

    tried a label

--- LearningTkinter.py
+++ LearningTkinter.py
@@ -1,4 +1,6 @@
 import tkinter as tk 
 
-window = tk.Tk() # Container for all widgets
-window.mainloop()
+window = tk.Tk() # Container for all widgets (text boxes, labels, and buttons)
+greeting = tk.Label(text="Hello, Tkinter")
+greeting.pack() 
+window.mainloop() # Event loop for listen for any kind of events 
