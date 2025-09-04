79adfef8a02f9032d81f0563a919d79e081d6d10
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-09-04 17:15:46 -0500

    created a button widget

--- LearningTkinter.py
+++ LearningTkinter.py
@@ -1,6 +1,16 @@
 import tkinter as tk 
 
 window = tk.Tk() # Container for all widgets (text boxes, labels, and buttons)
-greeting = tk.Label(text="Hello, Tkinter")
-greeting.pack() 
+label = tk.Label( # Only displaying, nothing else
+    text="Hello User!",
+    foreground = "#6495ED",
+    background = "#E6E6FA",
+    height = 10,
+    width = 20 ).pack()
+button = tk.Button(
+    text = "Click Here!",
+    height = 10,
+    width = 20,
+    foreground = "#6495ED").pack() # Won't display background color )
+
 window.mainloop() # Event loop for listen for any kind of events 
