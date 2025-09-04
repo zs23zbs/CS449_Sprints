3d355d67545b38268b1e06bedc203e1bf783081e
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-09-04 10:31:51 -0500

    All six tests pass

--- test_sample.py
+++ test_sample.py
@@ -28,7 +28,7 @@ class Test_Binary_Search:
         result = searchAlgor.BinarySearchAlgor(sort_list, 20, low, high)
         assert result, 5
     
-    def test_large_range(self): # Passes 
+    def test_large_range(self): # Pass
         list = [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 34, 45, 67, 89, 94, 101, 120, 495, 283, 596, 294, 300, 319, 314, 3, 6, 8]
         first_element = list[0]
         sort_list = sorted(list)
@@ -38,7 +38,7 @@ class Test_Binary_Search:
         result = searchAlgor.BinarySearchAlgor(sort_list, 596, low, high)
         assert result, 32
     
-    def test_no_key_found(self):  
+    def test_no_key_found(self):  # Pass
         list = [45, 684, 283, 749, 8, 10, 293]
         first_element = list[0]
         sort_list = sorted(list)
