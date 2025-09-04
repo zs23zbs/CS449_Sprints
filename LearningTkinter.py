b055788df2638cb7cba4b80075c970cdd88d2a78
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-09-04 17:34:41 -0500

    Tried an entry widget

--- LearningTkinter.py
+++ LearningTkinter.py
@@ -2,15 +2,18 @@ import tkinter as tk
 
 window = tk.Tk() # Container for all widgets (text boxes, labels, and buttons)
 label = tk.Label( # Only displaying, nothing else
-    text="Hello User!",
+    text="Hello User! Please enter your name below",
     foreground = "#6495ED",
-    background = "#E6E6FA",
+    background = "black",
     height = 10,
     width = 20 ).pack()
 button = tk.Button(
     text = "Click Here!",
     height = 10,
     width = 20,
-    foreground = "#6495ED").pack() # Won't display background color )
-
+    foreground = "#6495ED").pack() 
+entry = tk.Entry(
+    background = "#8FBC8B",
+    foreground = "#FFE4C4",
+    width = 20 ).pack()
 window.mainloop() # Event loop for listen for any kind of events 
