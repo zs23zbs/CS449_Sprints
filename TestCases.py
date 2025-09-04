908bcd64133e79636e77aed53501f5b4f552b38d
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-09-03 21:41:48 -0500

    Added first unit test

---
+++ TestCases.py
@@ -0,0 +1,7 @@
+import pytest
+import searchAlgor
+
+class Test_Linear_Search():
+    def test_small_range(self):
+        result = searchAlgor.LinearSearchAlgor(2, range(1,3))
+        assert (result, 1)
