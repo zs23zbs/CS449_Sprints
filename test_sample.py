d756695c39c64fd3254974f7237a2c8f92089ba5
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-09-03 22:16:41 -0500

    two tests pass

--- test_sample.py
+++ test_sample.py
@@ -4,3 +4,7 @@ class Test_Linear_Search:
     def test_small_range(self):
         result = searchAlgor.LinearSearchAlgor(2, range(1,3))
         assert result, 1
+
+    def test_large_range(self):
+        result = searchAlgor.LinearSearchAlgor(88, range(23, 125))
+        assert result, 65
