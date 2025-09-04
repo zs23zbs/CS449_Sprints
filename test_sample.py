ce29e972ae4b60ffbb1d2acc4774afd3fe220234
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-09-03 22:20:09 -0500

    saved file

--- test_sample.py
+++ test_sample.py
@@ -1,10 +1,14 @@
 import searchAlgor
 
-class Test_Linear_Search:
+# Using Python 3.9.6 for pytest to work, DON'T CHANGE Python: Select Interpreter 
+# command specifically for interpreter, executing the pytests is "pytest" itsef. 
+# command+Shift+P for fast track on terminal 
+
+class Test_Linear_Search: # Pass
     def test_small_range(self):
         result = searchAlgor.LinearSearchAlgor(2, range(1,3))
         assert result, 1
 
-    def test_large_range(self):
+    def test_large_range(self): # Pass
         result = searchAlgor.LinearSearchAlgor(88, range(23, 125))
         assert result, 65
