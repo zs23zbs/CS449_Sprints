2f61e80da86fb44706da9bd0786ff7b3a147cdf5
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-09-04 21:46:47 -0500

    working through basics of Tkinter

--- LearningTkinter.py
+++ LearningTkinter.py
@@ -1,19 +1,14 @@
 import tkinter as tk 
 
-window = tk.Tk() # Container for all widgets (text boxes, labels, and buttons)
-label = tk.Label( # Only displaying, nothing else
-    text="Hello User! Please enter your name below",
-    foreground = "#6495ED",
-    background = "black",
-    height = 10,
-    width = 20 ).pack()
-button = tk.Button(
-    text = "Click Here!",
-    height = 10,
-    width = 20,
-    foreground = "#6495ED").pack() 
-entry = tk.Entry(
-    background = "#8FBC8B",
-    foreground = "#FFE4C4",
-    width = 20 ).pack()
-window.mainloop() # Event loop for listen for any kind of events 
+window = tk.Tk() 
+
+frame1 = tk.Frame(master=window, width = 200, height = 100, bg = "#40E0D0")
+frame1.pack(fill=tk.BOTH, side = tk.LEFT, expand = True)
+
+frame2 = tk.Frame(master=window, width = 50, bg = "#87CEFA")
+frame2.pack(fill=tk.BOTH, side = tk.LEFT, expand = True)
+
+frame3 = tk.Frame(master=window, width=25, bg="#BA55D3")
+frame3.pack(fill=tk.BOTH, side= tk.LEFT, expand = True)
+
+window.mainloop()
