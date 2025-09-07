766a3a67ad9595fd1cf2c60146e8179fc8374fe2
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-09-06 20:55:43 -0500

    Trying geometry managers

--- LearningTkinter.py
+++ LearningTkinter.py
@@ -2,13 +2,15 @@ import tkinter as tk
 
 window = tk.Tk() 
 
-frame1 = tk.Frame(master=window, width = 200, height = 100, bg = "#40E0D0")
-frame1.pack(fill=tk.BOTH, side = tk.LEFT, expand = True)
-
-frame2 = tk.Frame(master=window, width = 50, bg = "#87CEFA")
-frame2.pack(fill=tk.BOTH, side = tk.LEFT, expand = True)
-
-frame3 = tk.Frame(master=window, width=25, bg="#BA55D3")
-frame3.pack(fill=tk.BOTH, side= tk.LEFT, expand = True)
+for i in range(3):
+    for j in range(3):
+        frame = tk.Frame(
+            master = window,
+            relief = tk.RAISED,
+            borderwidth = 1
+        )
+        frame.grid(row = i, column = j)
+        label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
+        label.pack()
 
 window.mainloop()
