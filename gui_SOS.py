05e9730e4fe65ed999acf9c9394b22581991167b
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-09-07 17:13:05 -0500

    Created board game display

--- gui_SOS.py
+++ gui_SOS.py
@@ -4,34 +4,29 @@ import tkinter as tk
 window = tk.Tk()
 window.title("SOS TicTacToe")
 
-"""Create the responsive window"""
-window.rowconfigure(5, minsize= 800, weight = 2)
-window.columnconfigure(5, minsize= 800, weight=2)
+"""Creating TicTacToe Board"""
+for i in range(5):
+    window.columnconfigure(i, weight=5, minsize= 50)
+    window.rowconfigure(i, weight=5, minsize= 50)
 
-"""TicTacToe Board widget"""
-class TicTacToeBoard(tk.Tk()):
-    def __init__(self):
-        super().__init__()
-        self._cells= {}
-    
-    def game_board_display(self):
-        display_frame = tk.Frame(master=self)
-        display_frame.pack(fill=tk.X)
-        self.display = tk.Label(
-            master= display_frame,
-            text="Ready?",
-            font = font.Font(size=10, weight="bold")
+    for j in range(1,6):
+        boardGame_frame = tk.Frame(
+            master=window,
+            relief= tk.SUNKEN,
+            bd=5,
         )
-        self.display.pack()
+        boardGame_frame.grid(row=i, column=j,padx=5, pady=5)
+        boardGame_label = tk.Label(master=boardGame_frame, width=5, height=5, text="")
+        boardGame_label.pack(padx=5, pady=5)
 
 """Board Size widget"""
 boardSize_frame = tk.Frame(master=window, bd=3, relief=tk.RAISED, bg="#008B8B")
 boardSize_label = tk.Label(master=boardSize_frame, text=" Board Size: 5", fg="black", bg="#E0FFFF")
-boardSize_label.grid(row=0, column=5, sticky="ne", padx=5, pady=5)
-boardSize_frame.grid(row=0, column=5, sticky="ne")
+boardSize_label.grid(row=0, column=6, sticky="ne", padx=5, pady=5)
+boardSize_frame.grid(row=0, column=6, sticky="ne")
 
 """Creating the mode widget"""
-mode_frame = tk.Frame(master=window, bd=5, relief=tk.RAISED, bg="#20B2AA")
+mode_frame = tk.Frame(master=window, bd=2, relief=tk.RAISED, bg="#20B2AA")
 mode_label = tk.Label(master=mode_frame, text=f"MODE:", fg="black", bg="#E0FFFF")
 game_modes = ["Simple Game", "General Game"]
 
@@ -46,10 +41,9 @@ for i, modes in enumerate(game_modes): # Iterate through the game_modes list to 
         variable=variable,
         value=modes,
         command = pick_mode
-    ).grid(row=i+1, column=0, sticky="ew", padx=6, pady=6)
+    ).grid(row=i+1, column=0, sticky="ew", padx=5, pady=5)
 
 mode_label.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
-mode_frame.grid(row=0, column=0, sticky="ew", padx=10, pady=10)
-
+mode_frame.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
 
 window.mainloop()
