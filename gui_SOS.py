b6c8427505e8ae2224417f0ee0318c369c695e5b
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-09-07 12:22:37 -0500

    Trying to fix radio buttons for mode

--- gui_SOS.py
+++ gui_SOS.py
@@ -9,9 +9,22 @@ window.rowconfigure(5, minsize= 800, weight = 2)
 window.columnconfigure(5, minsize= 800, weight=2)
 
 """Creating the mode widget"""
-mode_frame = tk.Frame(master=window, bd=5, relief=tk.RAISED, width=200, height=100, bg="red")
-game_modes = ["Simple Game", "General Game"]
-mode_label = tk.Label(master=mode_frame, text="GAME MODE:", fg="pink")
+mode_frame = tk.Frame(master=window, bd=5, relief=tk.RAISED, width=200, height=100, bg="white")
+game_modes = ["mode","Simple Game", "General Game"]
+mode_label = tk.Label(master=mode_frame, text=f"GAME MODE: {game_modes[0]}", fg="pink")
+
+def pick_mode():
+    mode_label.config(text=f"Select a Game Mode: {variable.get()}")
+variable = tk.StringVar(mode_frame, f"{game_modes[0]}")
+
+for modes in game_modes:
+    tk.Radiobutton(
+        master=mode_frame,
+        text=modes,
+        variable=variable,
+        value=modes,
+        command = pick_mode
+    ).grid(row=2, column=0)
 
 mode_label.grid(row=0, column=0, sticky="ne", padx=3, pady=3)
 mode_frame.grid(row=1, column=0)
