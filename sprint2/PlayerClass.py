1b2a63b85b2625c62a6b5a06c4dfb0330e608f41
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-10-14 14:14:20 -0500

    Added methods to player class (half)

---
+++ sprint2/PlayerClass.py
@@ -0,0 +1,9 @@
+"""Player Class"""
+
+class Player:
+    def __init__(self, color, letter_choice):
+        self.color = color
+        self.letter_choice = letter_choice
+    
+    def toggle_letter(self):
+        pass
