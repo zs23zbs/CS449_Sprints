959f45a4fa7464ee2b3ad3155051eb1ba9385520
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-10-14 14:35:29 -0500

    created toggle_letter method for player class

--- sprint2/PlayerClass.py
+++ sprint2/PlayerClass.py
@@ -1,9 +1,15 @@
 """Player Class"""
 
 class Player:
-    def __init__(self, color, letter_choice):
+    def __init__(self, color, letter_choice="S"):
         self.color = color
         self.letter_choice = letter_choice
     
-    def toggle_letter(self):
-        pass
+    def toggle_letter(self): # for when the toggle changes between S and O letter 
+        current_player = Player() 
+        current_letter = Player()
+
+        if current_player.letter_choice == "S":
+            current_letter.letter_choice = "O"
+        elif current_player.letter_choice == "O":
+            current_letter.letter_choice = "S"
