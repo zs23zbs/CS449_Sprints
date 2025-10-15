0a88300e0f8d10b15ca625e50e9249a11e1af5bd
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-10-14 22:45:09 -0500

    Fixed Player class and added assurance for debugging

--- sprint2/PlayerClass.py
+++ sprint2/PlayerClass.py
@@ -6,8 +6,17 @@ class Player:
         self.letter_choice = letter_choice
     
     def toggle_letter(self): # for when the toggle changes between S and O letter 
-        pass
         # Has to check what the current letter choice is 
         # if current letter is S, then must change to O 
         # if current letter is O, then change to S 
-        
+
+        if self.letter_choice == "S":
+            self.letter_choice = "O"
+        elif self.letter_choice == "O":
+            self.letter_choice = "S"
+
+        return self.letter_choice
+    
+    def __str__(self): # Just to help with debugging 
+        return f"{self.color} player placing {self.letter_choice}"
+
