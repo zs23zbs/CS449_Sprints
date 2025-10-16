71487240ae014272a107052190ed0b1ab82592f9
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-10-16 10:27:55 -0500

    completed is_cell_empty method

--- sprint2/BoardClass.py
+++ sprint2/BoardClass.py
@@ -6,10 +6,9 @@
 
 class Board:
     def __init__(self, board_size): 
-        # Initilizes a  2d nested list board with the input board size by user 
+        # Initializes a  2d nested list board with the input board size by user 
         self.board_size = board_size
         
-
         self.game_board = []
         for i in range(board_size): 
             row = []
@@ -17,8 +16,12 @@ class Board:
                 row.append(None)
             self.game_board.append(row)
     
-    def is_cell_empty(self, row, col):
-        pass
+    def is_cell_empty(self, row, col): 
+        # Before player makes a move, system check if it is empty 
+        if self.game_board[row][col] == None:
+            return True
+        else:
+            return False
 
     def place(self, row, col, letter, color): 
         pass
@@ -26,7 +29,7 @@ class Board:
     def reset(self):
         pass
 
-    def __str__(self):
+    def __str__(self): #Just to be able to see the board itself
         return f"{self.game_board}" 
 
 new_game_board = Board(3)
