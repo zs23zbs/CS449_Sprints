3b27cffddec37d8e4e74d7dc28985ba1433b4ec8
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-10-17 17:50:36 -0500

    Changeing the constructor in game logic class

--- sprint2/BoardClass.py
+++ sprint2/BoardClass.py
@@ -4,7 +4,7 @@
 # Ensure valid moves
 
 class Board:
-    def __init__(self, board_size): #===Works!===
+    def __init__(self, board_size):
         # Initializes a  2d nested list board with the input board size by user 
         self.board_size = board_size
         
@@ -15,14 +15,14 @@ class Board:
                 row.append(None)
             self.game_board.append(row)
     
-    def is_cell_empty(self, row, col):  #===Works!===
+    def is_cell_empty(self, row, col):
         # Before player makes a move on cell, system checks if it is empty 
         if self.game_board[row][col] == None:
             return True
         else:
             return False
 
-    def place(self, row, col, letter, color): #===Works!===
+    def place(self, row, col, letter, color): 
         # Updates the cell whichever player chooses to make a move
         if row not in range(0, self.board_size) or col not in range(0, self.board_size):
             return False
@@ -34,7 +34,7 @@ class Board:
 
         return True 
 
-    def reset(self): #===Check===
+    def reset(self):
         #Reset the the board, all cells are empty once more
         self.game_board = []
         for i in range(self.board_size): 
--- sprint2/game_logic.py
+++ sprint2/game_logic.py
@@ -1,8 +1,30 @@
+from BoardClass import Board
+from PlayerClass import Player
+
 # Game Logic Class
 # Control the flow of the game 
     # Managing Turns 
     # Move controls 
 
 class GameLogic:
-    def __init__(self):
-        pass 
+    def __init__(self, board, mode):
+        self.board = board
+        self.mode = mode
+
+        # Created Objects
+        self.player_blue = Player("Blue", "S")
+        self.player_red = Player("Red", "S")
+
+        # Start with Blue player's turn 
+        self.current_turn = self.player_blue
+
+    def make_move(self, row, col): # Very unsure about method 
+        if self.board_game.is_cell_empty(row, col) == True:
+            self.current_turn = self.letter, self.color # set to current player 
+            self.board_game.place(row, col, self.letter, self.color) # place a move on a cell
+            self.current_turn = "Red" # change the player color to indicate "switching to other player"
+        else:
+            return False
+        
+    def switch_turn():
+        pass
