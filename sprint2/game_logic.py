95dd2f4172627bb95340ef9bb6523d54ff7e68f3
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-10-17 17:55:18 -0500

    Fixed make move method, and created switch turn method

--- sprint2/game_logic.py
+++ sprint2/game_logic.py
@@ -7,8 +7,8 @@ from PlayerClass import Player
     # Move controls 
 
 class GameLogic:
-    def __init__(self, board, mode):
-        self.board = board
+    def __init__(self, game_board_size, mode):
+        self.board = Board(game_board_size)
         self.mode = mode
 
         # Created Objects
@@ -19,12 +19,17 @@ class GameLogic:
         self.current_turn = self.player_blue
 
     def make_move(self, row, col): # Very unsure about method 
-        if self.board_game.is_cell_empty(row, col) == True:
-            self.current_turn = self.letter, self.color # set to current player 
-            self.board_game.place(row, col, self.letter, self.color) # place a move on a cell
-            self.current_turn = "Red" # change the player color to indicate "switching to other player"
-        else:
+        if self.board.is_cell_empty(row, col):
+            self.board_game.place(row, col, self.current_turn.letter_choice, self.current_turn.color)
+            self.switch_turn()
+            return True 
+        else: #Move is invalid if the cell is occupied 
             return False
         
-    def switch_turn():
-        pass
+    def switch_turn(self):
+        if self.current_turn == self.player_blue:
+            self.current_turn = self.player_red
+        else:
+            self.current_turn = self.player_blue
+
+    
