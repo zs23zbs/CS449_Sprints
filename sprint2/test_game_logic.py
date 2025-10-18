55ef321b4228050c80744125660bdf9c0338e1ef
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-10-17 20:12:26 -0500

    Created + tested second AI generated tests

--- sprint2/test_game_logic.py
+++ sprint2/test_game_logic.py
@@ -10,3 +10,14 @@ def test_make_move_and_switch_turn():
     assert successful_move is True
     assert game.board.game_board[0][0]==("S", "Blue")
     assert game.current_turn.color == "Red"
+
+def test_invalid_move_doesnt_change_turn():
+    game = GameLogic(3, "Simple")
+
+    game.make_move(0,0)
+    previous_turn = game.current_turn.color
+
+    move_is_successful = game.make_move(0,0)
+
+    assert move_is_successful is False
+    assert game.current_turn.color == previous_turn
