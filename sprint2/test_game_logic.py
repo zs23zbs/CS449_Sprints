97868dab02e6a0ba76b9767316b1c657917c7b3b
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-10-17 20:07:13 -0500

    Created and tested first automated AI generated test

--- sprint2/test_game_logic.py
+++ sprint2/test_game_logic.py
@@ -2,3 +2,11 @@ import pytest
 from game_logic import GameLogic
 
 """AI Generated Automated Tests"""
+def test_make_move_and_switch_turn(): 
+    game = GameLogic(3, "Simple")
+
+    successful_move = game.make_move(0,0)
+
+    assert successful_move is True
+    assert game.board.game_board[0][0]==("S", "Blue")
+    assert game.current_turn.color == "Red"
