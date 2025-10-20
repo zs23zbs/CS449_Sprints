9f4a2b18e007ba480af83868dd45d27fa442cdf7
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-10-20 18:56:35 -0500

    Added mode widget

--- sprint2/sos_gui.py
+++ sprint2/sos_gui.py
@@ -19,4 +19,25 @@ boardSize_label = tk.Label(master=boardSize_frame, text="Game Board Size: 5x6", 
 boardSize_label.grid(row=0, column=6, padx=5, pady=5)
 boardSize_frame.grid(row=0, column=6, padx=5, pady=5)
 
+"""Creating the mode widget"""
+game_modes = ["Simple Game", "General Game"] # List of options for user to pick for game mode / creating a radio button
+mode_frame = tk.Frame(master=window, bd=5, relief=tk.RAISED, bg="#20B2AA")
+mode_label = tk.Label(master=mode_frame, text=f"MODE:", fg="black", bg="#E0FFFF")
+
+def pick_mode(): # Fuction for selecting whichever game mode for the radio button
+    mode_label.config(text=f"MODE:")
+variable1 = tk.StringVar(mode_frame, f"{game_modes[0]}")
+
+for i, modes in enumerate(game_modes): # Iterate through the game_modes list to display the different game versions the user may want to play in 
+    tk.Radiobutton(
+        master=mode_frame,
+        text=modes,
+        variable=variable1,
+        value=modes,
+        command = pick_mode
+    ).grid(row=i+1, column=0,padx=4, pady=4)
+
+mode_label.grid(row=0, column=0)
+mode_frame.grid(row=0, column=0)
+
 window.mainloop()
