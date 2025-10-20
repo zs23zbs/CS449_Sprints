fbc7a205076e6a43da77c04f28516f0bd4430f8d
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-10-20 18:56:00 -0500

    Added board size widget from original to new

--- sprint2/sos_gui.py
+++ sprint2/sos_gui.py
@@ -11,4 +11,12 @@ board_buttons = []
 board_size = tk.IntVar(value=3)
 mode = tk.StringVar(value="Simple Game")
 
+"""Board Size widget""" 
+boardSize_frame = tk.Frame(master=window, bd=5, relief=tk.RAISED, bg="#008B8B") 
+boardSize_label = tk.Label(master=boardSize_frame, text="Game Board Size: 5x6", fg="black", bg="#E0FFFF")
+
+# Format the board size widget 
+boardSize_label.grid(row=0, column=6, padx=5, pady=5)
+boardSize_frame.grid(row=0, column=6, padx=5, pady=5)
+
 window.mainloop()
