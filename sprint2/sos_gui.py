171becd4856ea8be70cadf0e6c1e495d523df9d1
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-10-20 18:53:08 -0500

    Create variables for game logic

--- sprint2/sos_gui.py
+++ sprint2/sos_gui.py
@@ -1,8 +1,14 @@
-from game_logic import GameLogic
+from game_logic import GameLogic # Import GameLogic (backend)
 import tkinter as tk 
 
 """Initiate and create window with title"""
 window = tk.Tk()
 window.title("SOS TicTacToe")
+
+"""Variables for Game"""
+game = None
+board_buttons = []
+board_size = tk.IntVar(value=3)
+mode = tk.StringVar(value="Simple Game")
 
 window.mainloop()
