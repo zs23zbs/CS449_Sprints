7a85e7ed4a7de2c61c5342108db756076a1750e7
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-11-03 03:57:10 -0600

    removed player letter choice

--- sprint2/player_class.py
+++ sprint2/player_class.py
@@ -1,11 +1,9 @@
 class Player:
-    def __init__(self, color, letter_choice="S"):
+    def __init__(self, color):
         """Initialize Player objects
         
         Args: 
         color (str): The color for identifying players (Red or Blue)
-        letter_choice (str): The letter or label that players use to play SOS game (S or O) 
         """
         
         self.color = color
-        self.letter_choice = letter_choice
