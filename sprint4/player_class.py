3cf06dedfb0ff8fb3953711c13ef305d69219d8d
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-11-09 15:44:02 -0600

    started with refactoring player file and adding the human and computer player classes

--- sprint4/player_class.py
+++ sprint4/player_class.py
@@ -1,1 +1,22 @@
+"""Need to refactor Player Class to include Human and Computer Player"""
+from abc import ABC, abstractmethod
 
+class Player(ABC):
+    @abstractmethod
+    def __init__(self, color):
+        """Initialize Player objects
+        
+        Args: 
+        color (str): The color for identifying players (Red or Blue)
+        """
+    
+        self.color = color
+
+    def next_move(self, board, available_moves):
+        pass 
+
+class HumanPlayer(Player):
+    pass
+
+class ComputerPlayer(Player):
+    pass
