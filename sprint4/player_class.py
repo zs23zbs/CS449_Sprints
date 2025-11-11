f67e7d0181331bf9e04883139149cebcdeaf11f8
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-11-11 16:26:09 -0600

    implement iteration through board and see which ones are empty to make a move on

--- sprint4/player_class.py
+++ sprint4/player_class.py
@@ -1,5 +1,6 @@
 """Need to refactor Player Class to include Human and Computer Player"""
 from abc import ABC, abstractmethod
+from board_class import Board
 
 class Player(ABC):
     def __init__(self, color):
@@ -30,4 +31,11 @@ class ComputerPlayer(Player):
     def next_move(self, board):
         """Looks through the current state of game board and make a valid move"""
         available_moves = [] # store the moves (tuple of cell and letter) that the computer player can make
-        
+
+        # Iterate through all the rows and columns of board game 
+        for row in board:
+            for col in board:
+                # if the cell in board is empty, make cell available for a move
+                if Board.is_cell_empty(row, col) == None: 
+                    available_moves.append(row, col, "S")
+                    available_moves.append(row, col, "O")
