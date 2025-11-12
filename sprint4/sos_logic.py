d7bbfc022247ef2c4ab16ecedecfd2258bf23157
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-11-12 09:51:39 -0600

    created the logic for sos game file

---
+++ sprint4/sos_logic.py
@@ -0,0 +1,15 @@
+from board_class import Board
+from player_class import Player
+from abc import ABC, abstractmethod
+
+# Must refactor!
+
+class SOSLogic(ABC):
+    def __init__(self, game_board_size, player_blue, player_red):
+        """Initialize instance for logic of game"""
+        self.board = Board(game_board_size)
+        self.player_blue = player_blue 
+        self.player_red = player_red
+        self.current_turn = player_blue
+        self.score_count = {self.player_blue.color : 0, self.player_red.color: 0}
+    
