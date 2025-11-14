4e1d1b0571c48ee04dd23342f6cd50294a9c3b5e
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-11-14 14:39:18 -0600

    completed the constructor method

--- sprint4/gui.py
+++ sprint4/gui.py
@@ -2,7 +2,25 @@ import tkinter as tk
 
 class SOSGame():
     def __init__(self):
-        pass
+        """Initilize instances/vairables for SOS game"""
+
+        # Create start_menu instances 
+        self.start_menu = tk.Tk()
+        self.start_menu.title("SOS Start Menu")
+        self.start_menu.config(bg="#008B8B")
+
+        # Varaibles for start_menu buttons (board size and mode)
+        self.board_size = tk.Var(value=3) # default/smallest board size
+        self.mode = tk.StringVar(value="Simple Game") # default game mode
+
+        # To start with for letter choices
+        self.red_letter_choice = tk.StringVar(value="S")
+        self.blue_letter_choice = tk.StringVar(value="O")
+        self.red_selection_label = None
+        self.blue_selection_label = None
+        
+        self.create_start_menu()
+        self.start_menu.mainloop() # immediately straight to start menu
 
     def start_game(self):
         pass
@@ -24,3 +42,6 @@ class SOSGame():
 
     def end_game(self):
         pass
+
+if __name__ == "__main__":
+    SOSGame()
