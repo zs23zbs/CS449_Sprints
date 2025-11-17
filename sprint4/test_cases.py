31e601b345a9f62918e85a12479601e92ee6df44
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-11-17 09:14:16 -0600

    all four tests cases pass

--- sprint4/test_cases.py
+++ sprint4/test_cases.py
@@ -1,6 +1,7 @@
 import pytest 
 from board_class import Board
 from player_class import ComputerPlayer
+import random
 
 def test_computer_finds_horizontal_win():
     """Tests for Computer player to make a S-O-S horizontal win"""
@@ -49,3 +50,25 @@ def test_computer_player_find_vertical_win():
 
     # Assert that the move is completed
     assert move == (5, 4, "S")
+
+def test_computer_does_random_move(monkeypatch):
+    """Tests the computer player to make a random move when no immediate SOS can be done"""
+
+    board = Board(3) 
+    computer_player = ComputerPlayer("Red")
+
+    # Fill two cells for no accident wins
+    board.place(0, 0, "O", "Red")
+    board.place(2, 2, "0", "Blue")
+
+    # Mock random to pick a predictable move (0, 1, "S")
+    def mock_choice(seq):
+        return (0, 1, "S")
+    
+    monkeypatch.setattr(random, "choice", mock_choice)
+
+    move = computer_player.get_move(board)
+
+    # Assert that the move is the one the mocked random returns 
+    assert move == (0, 1, "S")
+    assert board.is_cell_empty(0,1)
