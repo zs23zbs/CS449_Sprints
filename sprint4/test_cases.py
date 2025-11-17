6a79a050204ef09a81da4fc94cc424d709697590
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-11-17 09:06:13 -0600

    all computer win test cases pass

--- sprint4/test_cases.py
+++ sprint4/test_cases.py
@@ -9,7 +9,7 @@ def test_computer_finds_horizontal_win():
     board = Board(3) 
     computer_player = ComputerPlayer("Red")
 
-    # Create the winning pattern 
+    # Create the winning (horizontal) pattern 
     board.place(0, 0, "S", "Blue")
     board.place(0, 1, "O", "Red")
 
@@ -17,3 +17,35 @@ def test_computer_finds_horizontal_win():
 
     # Assert that the move is completed
     assert move == (0, 2, "S")
+
+def test_computer_find_diagonal_win():
+    """Tests for Computer player to make an S-O-S diagonal win"""
+
+    # Set up the board 
+    board = Board(5)
+    computer_player = ComputerPlayer("Red")
+
+    # set up the winning (diagonal) pattern 
+    board.place(1, 1, "S", "Blue")
+    board.place(2, 2, "O", "Red")
+
+    move = computer_player.get_move(board)
+
+    # Assert that the move is completed
+    assert move == (3, 3, "S")
+
+def test_computer_player_find_vertical_win():
+    """Tests for Computer player to make a vertical S-O-S win"""
+
+    # set up board object
+    board = Board(6)
+    computer_player = ComputerPlayer("Blue")
+
+    # Set up the winning (vertical) pattern
+    board.place(5, 2, "S", "Red")
+    board.place(5, 3, "O", "Blue")
+
+    move = computer_player.get_move(board)
+
+    # Assert that the move is completed
+    assert move == (5, 4, "S")
