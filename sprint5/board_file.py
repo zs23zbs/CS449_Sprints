8381066d3b99cd2f6cdd556890c30395ccc80b5d
Zharinna Subido <zbsxph@mail.umkc.edu>
2025-11-20 19:30:18 -0600

    refactored check_for_SOS method

--- sprint5/board_file.py
+++ sprint5/board_file.py
@@ -1,1 +1,109 @@
+# Must refactor before implementing Sprint5 
+# Follow Basic Refactoring Principles 
+class Board():
+    def __init__(self, board_size):
+        """Initiate the 2d nested list for board"""
+        self.board_size = board_size
+        self.game_board = []
 
+        for i in range(board_size): 
+            row = []
+            for j in range(board_size):
+                row.append(None)
+            self.game_board.append(row)
+
+    def is_cell_empty(self, row, col):
+        """Checks if board cell is empty and checks boundaries"""
+        if not (0 <= row < self.board_size and 0 <= col < self.board_size):
+            return False
+        return self.game_board[row][col] is None 
+
+    def place(self, row, col, letter, color):
+        """Place a ltter on the game board"""
+        if not self.is_cell_empty(row, col):
+            return False
+        
+        self.game_board[row][col] = (letter, color)
+
+        return True 
+
+    def unplace(self, row, col):
+        """Remove the content of a cell, make it empty again"""
+        if 0 <= row < self.board_size and 0 <= col < self.board_size:
+            self.game_board[row][col] = None 
+
+    def get_letter(self, row, col):
+        """Returns the letter (S or O) from a cell or None"""
+        if 0 <= row < self.board_size and 0 <= col < self.board_size:
+            content = self.game_board[row][col]
+            if content is not None: 
+                return content[0] 
+        return None
+ 
+    def check_for_SOS(self, row, col):
+        """Detects an SOS and return line coordinates"""
+        directions = [
+            (-1, 0), (1, 0),  # Vertical (N, S)
+            (0, -1), (0, 1),  # Horizontal (W,E)
+            (-1, -1), (1, 1), # Diagonal 1 (NW, SE)
+            (-1, 1), (1, -1) # Diagonal 2 (NE, SW) 
+        ]
+  
+        found_SOS= [] 
+        current_letter = self.get_letter(row, col)
+
+        if current_letter is None: 
+            return found_SOS
+
+        for rd, cd in directions: # row direction = rd, cd = column direction, refactors to recognize pattern not current letter
+
+            # Current cell is the center O 
+            s1_r, s1_c = (row + rd), (col + cd) 
+            s2_r, s2_c = (row - rd), (col -cd) 
+
+            s1 = self.get_letter(s1_r, s1_c)
+            s2 = self.get_letter(s2_r, s2_c)
+
+            if current_letter == "O" and s1 == "S" and s2 == "S":
+                line_tuple1 = ((s1_r, s1_c), (row, col), (s2_r, s2_c)) 
+                found_SOS.append(line_tuple1)
+
+            # Current cell is the first S 
+            o1_r, o1_c = (row + rd), (col + cd) 
+            s1_r, s1_c = row + (2 * rd), col + (2 * cd)
+
+            o1 = self.get_letter(o1_r, o1_c)
+            s1 = self.get_letter(s1_r, s1_c)
+
+            if current_letter == "S" and o1 == "O" and s1 == "S":
+                line_tuple2 = ((row, col), (o1_r, o1_c), (s1_r, s1_c))
+                found_SOS.append(line_tuple2)
+
+            # Current cell is the last S 
+            s2_r, s2_c = row - (2 * rd), col - (2 * cd)
+            o2_r, o2_c = (row - rd), (col- cd)
+
+            s2 = self.get_letter(s2_r, s2_c)
+            o2 = self.get_letter(o2_r, o2_c)
+
+            if current_letter == "S" and s2 == "S" and o2 == "O":
+                line_tuple3 = ((s2_r, s2_c), (o2_r, o2_c), (row, col))
+                found_SOS.append(line_tuple3)
+
+        return found_SOS
+    
+    def reset(self):
+        """Resets the game board, all cells empty"""
+        self.game_board = []
+        for i in range(self.board_size): 
+            row = []
+            for j in range(self.board_size):
+                row.append(None)
+            self.game_board.append(row)
+    
+    def is_full(self):
+        """Checks if the game board is filled"""
+        for row in self.game_board:
+            if None in row:
+                return False
+        return True 
